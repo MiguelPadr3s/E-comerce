07b33257db78276931bcdda9c65e64a2b085aef8
MiguelPadr3s <Miguelpadresm@hotmail.com>
2025-07-23 10:36:29 -0700

    cambios

--- Ecomerce/src/components/Courses.jsx
+++ Ecomerce/src/components/Courses.jsx
@@ -1,34 +1,54 @@
 import React from 'react'
 import Title from './Title';
 import { coursesSecItems } from '../constant/data';
+
 const Courses = () => {
     return (
         <section className="section">
             <div className="container">
                 {/* Title */}
-                <Title title="Our Courses" text="Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in." link="View All" />
+                <Title 
+                    title="Our Courses" 
+                    text="Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in." 
+                    link="View All" 
+                />
+
                 {/* card wrapper */}
-                <div className="">
+                <div className="grid gap-5 lg:grid-cols-2 mt-8 lg:mt-16">
                     {coursesSecItems.map((item) => (
                         //card
-                        <div className="">
+                        <div className="bg-blue-200 p-7 lg:p-10 rounded-[10px]" key={item.id}>
                             {/* img */}
                             <div className="">
-                                <img src={item.img} alt={item.title} width={560} height={266} />
+                                <img 
+                                    src={item.img} 
+                                    alt={item.title} 
+                                    width={560} 
+                                    height={266} 
+                                    className="w-full h-full object-cover rounded-[10px]"
+                                    />
                             </div>
                             {/* Content */}
-                            <div className="">
+                            <div className="mt-6">
                                 {/* tags */}
-                                <div className="">
+                                <div className="flex items-center flex-wrap gap-2.5">
                                     {item.tags.map((tag,index) => (
-                                        <span className=""></span>
+                                        <span className="border border-blue-800 p-2 rounded-md bg-blue-100 mr-2" key={index}>
+                                            {tag.tag}
+                                        </span>
                                     ))}
+                                    <p className="ml-auto font-medium">{item.instructor}</p>
                                 </div>
                                 {/* meta data */}
-                                <div className=""></div>
+                                <div className="my-6 space-y-1.5">
+                                    <h4>{item.title}</h4>
+                                    <p>{item.text}</p>
+                                </div>
                             </div>
                             {/* btn */}
-                            <button className=""></button>
+                            <button className="bg-white-97 w-full p-3.5 rounded-md hover:text-blue-500 border border-white-97 hover:border-blue-500 transition">
+                                Get it Now!
+                            </button>
                         </div>
                     ))}
                 </div>
